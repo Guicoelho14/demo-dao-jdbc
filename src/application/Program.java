cb79230b99f13844c9d3ca9646d5322fadc55209
[Guicoelho14] <[guiwanderley@msn.com]>
2025-03-10 22:55:26 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDAO;
@@ -18,6 +19,14 @@ public class Program {
 		Seller seller = sellerDAO.findById(3);
 		
 		System.out.println(seller);
+		
+		System.out.println("\n=== TEST 2: seller findByDepartment ===");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDAO.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
 	}
 
 }

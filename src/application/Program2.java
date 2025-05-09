43049bd5bf4bddee4cb6e7f52efbcfe3fd149fee
[Guicoelho14] <[guiwanderley@msn.com]>
2025-05-08 22:30:55 -0300

    insert department implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -38,12 +38,12 @@ public class Program2 {
 			System.out.println(obj);
 		}
 		
-		/*System.out.println("\n=== TEST 4: seller insert ===");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-		sellerDAO.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
+		System.out.println("\n=== TEST 3: department insert ===");
+		Department newDepartment = new Department(null, "TVs");
+		departmentDAO.insert(newDepartment);
+		System.out.println("Inserted! New id = " + newDepartment.getId());
 		
-		System.out.println("\n=== TEST 5: seller update ===");
+		/*System.out.println("\n=== TEST 5: seller update ===");
 		seller = sellerDAO.findById(1);
 		seller.setName("Martha Waine");
 		sellerDAO.update(seller);
